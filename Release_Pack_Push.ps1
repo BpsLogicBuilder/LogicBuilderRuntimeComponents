95e1e5203b0927b61bcae9c17871548e41ec26ce
BlaiseD <btaylor@bPolicySolutions.com>
2020-07-20 06:53:34 -0400

    PWSL string formats.

--- Release_Pack_Push.ps1
+++ Release_Pack_Push.ps1
@@ -1,13 +1,13 @@
 $scriptName = $MyInvocation.MyCommand.Name
 
-Write-Host "Owner ${$Env:REPO_OWNER}"
-Write-Host "Repository ${$Env:REPO}"
+Write-Host "Owner ${Env:REPO_OWNER}"
+Write-Host "Repository ${Env:REPO}"
 
 $PROJECT_PATH = ".\$($Env:PROJECT_NAME)\$($Env:PROJECT_NAME).csproj"
 $NUGET_PACKAGE_PATH = ".\artifacts\$($Env:PROJECT_NAME).$($Env:VERSION_NUMBER).nupkg"
 
-Write-Host "Project Path ${$PROJECT_PATH}"
-Write-Host "Package Path ${$NUGET_PACKAGE_PATH}"
+Write-Host "Project Path ${PROJECT_PATH}"
+Write-Host "Package Path ${NUGET_PACKAGE_PATH}"
 
 if ($Env:REPO_OWNER -ne "BlaiseD") {
     Write-Host "${scriptName}: Runs on BlaiseD repositories."
